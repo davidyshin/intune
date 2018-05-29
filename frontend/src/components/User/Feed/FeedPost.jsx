6c87e84ed8c52393eb13133c65a4a64d71cd5b29
davidyshin <davidshin@ac.c4q.nyc>
2018-05-29 16:31:12 -0400

    production test

--- frontend/src/components/User/Feed/FeedPost.jsx
+++ frontend/src/components/User/Feed/FeedPost.jsx
@@ -29,13 +29,15 @@ class FeedPost extends Component {
     return feedPost && author ? (
       <div className="feed-post-container">
         <SpotifyPlayer size={size} uri={feedPost.spotify_uri} />
-        <div className="feed-post-caption">
-          <a className="feed-post-author-link">
+        <div className="feed-post-author">
+          <img className="feed-post-profile_pic" src={author.profile_pic} />
+
+          <a href={author.spotify_url} className="feed-post-author-link">
             {' '}
-            href={author.spotify_url}>{' '}
             <h3> {author.name ? author.name : author.spotifyid} </h3>
           </a>
-          <img src={author.profile_pic} />
+        </div>
+        <div className="feed-post-caption">
           <p> {feedPost.dates}</p>
           <p> {feedPost.caption}</p>
         </div>
