fa87b8b3c4624f896167f26972dcd0901d62887c
davidyshin <davidshin@ac.c4q.nyc>
2018-05-29 16:36:28 -0400

    production test

--- frontend/src/components/User/Feed/FeedPost.jsx
+++ frontend/src/components/User/Feed/FeedPost.jsx
@@ -29,17 +29,26 @@ class FeedPost extends Component {
     return feedPost && author ? (
       <div className="feed-post-container">
         <SpotifyPlayer size={size} uri={feedPost.spotify_uri} />
-        <div className="feed-post-author">
-          <img className="feed-post-profile_pic" src={author.profile_pic} />
+        <div className="feed-post-info">
+          <div className="feed-post-author">
+            <img
+              className="feed-post-profile_pic"
+              src={
+                author.profile_pic
+                  ? author.profile_pic
+                  : 'https://www.drupal.org/files/profile_default.png'
+              }
+            />
 
-          <a href={author.spotify_url} className="feed-post-author-link">
-            {' '}
-            <h3> {author.name ? author.name : author.spotifyid} </h3>
-          </a>
-        </div>
-        <div className="feed-post-caption">
-          <p> {feedPost.dates}</p>
-          <p> {feedPost.caption}</p>
+            <a href={author.spotify_url} className="feed-post-author-link">
+              {' '}
+              <h3> {author.name ? author.name : author.spotifyid} </h3>
+            </a>
+          </div>
+          <div className="feed-post-caption">
+            <p> {feedPost.dates}</p>
+            <p> {feedPost.caption}</p>
+          </div>
         </div>
       </div>
     ) : (
