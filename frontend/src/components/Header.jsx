b68b436218901fc7ae0b9f6c42a17601fffdad43
davidyshin <davidshin@ac.c4q.nyc>
2018-05-30 12:39:20 -0400

    zindex of usercontainer decremented so that the search suggestions go over it

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -63,7 +63,7 @@ class Header extends Component {
                             : 'rgba(0,0,0,.75)',
                           width: '99%',
                           height: '100%',
-                          zIndex: 999
+                          zIndex: '999'
                         }}
                       >
                         <div className="user-profile-pic">
